3ac4eaf34e8f7a03f242e90cfbf5b67f41141186
Andreas Bjørsvik <andreas.bjorsvik@gmail.com>
2021-09-01 12:44:52 +0200

    First edit

---
+++ UART/UART/main.c
@@ -0,0 +1,20 @@
+/*
+ * UART.c
+ *
+ * Created: 01.09.2021 12:40:28
+ * Author : andre
+ */ 
+
+#include <avr/io.h>
+
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+		
+    }
+}
+
