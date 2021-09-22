5d4e4596c7cc582e0eef69eb82eb8858d1f93443
patricab <patric.berthelsen@gmail.com>
2021-09-22 11:04:54 +0200

    Added external memory library

---
+++ node1/include/ext.h
@@ -0,0 +1,22 @@
+#ifndef EXT_H
+#define EXT_H
+
+/**
+ * @brief Write to external memory
+ * 
+ * @param mem_adress Memory adress i external RAM
+ * @param adress Internal adress of memory partition
+ * @param data Data to be written
+ */
+void ext_write(char mem_adress, char adress, char data);
+
+/**
+ * @brief Read from external memory
+ * 
+ * @param mem_adress Memory adress i external RAM
+ * @param adress Internal adress of memory partition
+ * @return char Data to be read
+ */
+char ext_read(char mem_adress, char adress);
+
+#endif
