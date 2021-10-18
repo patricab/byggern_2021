788c5a20dd35db6aa570089b2e28da402c51825a
patricab <patric.berthelsen@gmail.com>
2021-10-18 12:37:37 +0200

    Working compile. Hope it stays that way

--- node2/src/main.c
+++ node2/src/main.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 #include <stdarg.h>
-#include "uart_and_printf/uart.h"
-#include "uart_and_printf/printf-stdarg.h"
 
+// #include "sam/sam3x/include/sam3x8e.h"
+// #include "sam/sam3x/include/component/component_pio.h"
 #include "sam.h"
-#include <bit.h>
+// #include <bit.h>
 
 
 int main()
@@ -16,19 +16,22 @@ int main()
     // configure_uart();
 
     /* Enable PIO on bit PC2(D0) */
-    set_bit(PIOC->PIO_PER, PIO_PC2);
+    // set_bit(PIOC->PIO_PER, PIO_PC2);
+    PIOC->PIO_PER |= (1 << PIO_PC2);
     /* Set peripheral A on PC2(D0) */
-    clear_bit(PIOC->PIO_ABSR, PIO_PC2);
+    // clear_bit(PIOC->PIO_ABSR, PIO_PC2);
+    PIOC->PIO_ABSR |= (0 << PIO_PC2);
     /* Set output enable on PC2(D0) */
-    set_bit(PIOC->PIO_OER, PIO_PC2);
+    // set_bit(PIOC->PIO_OER, PIO_PC2);
+    PIOC->PIO_OER |= (1 << PIO_PC2);
 
     while (1)
     {
         /* Toggle PC2(D0) */
-        set_bit(PIOC->PIO_SODR, PIO_PC2);
+        PIOC->PIO_SODR |= (1 << PIO_PC2);
         int i = 0;
         while (i < 10000) {i++;}
-        set_bit(PIOC->PIO_CODR, PIO_PC2);
+        PIOC->PIO_CODR |= (1 << PIO_PC2);
     }
     
 }
