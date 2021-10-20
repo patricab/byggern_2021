18a8b000eb857407c6f5f5c8571d04a7960722dd
patricab <patric.berthelsen@gmail.com>
2021-10-20 14:19:32 +0200

    WIP

--- node2/src/main.c
+++ node2/src/main.c
@@ -4,6 +4,7 @@
 // #include "sam/sam3x/include/sam3x8e.h"
 // #include "sam/sam3x/include/component/component_pio.h"
 #include "sam.h"
+#include "uart.h"
 // #include <bit.h>
 
 
@@ -13,25 +14,26 @@ int main()
 
     WDT->WDT_MR = WDT_MR_WDDIS; //Disable Watchdog Timer
 
-    // configure_uart();
+    configure_uart();
 
-    /* Enable PIO on bit PC2(D0) */
-    // set_bit(PIOC->PIO_PER, PIO_PC2);
-    PIOC->PIO_PER |= (1 << PIO_PC2);
-    /* Set peripheral A on PC2(D0) */
-    // clear_bit(PIOC->PIO_ABSR, PIO_PC2);
-    PIOC->PIO_ABSR |= (0 << PIO_PC2);
-    /* Set output enable on PC2(D0) */
-    // set_bit(PIOC->PIO_OER, PIO_PC2);
-    PIOC->PIO_OER |= (1 << PIO_PC2);
+    // /* Disable pull-up on bit PC2(D0) */
+    // PIOC->PIO_PUDR |= PIO_PC2;
+    // /* Enable PIO on bit PC2(D0) */
+    // PIOC->PIO_PDR |= PIO_PC2;
+    // /* Set peripheral A on PC2(D0) */
+    // PIOC->PIO_ABSR &= ~PIO_PC2;
+    // /* Set output enable on PC2(D0) */
+    // PIOC->PIO_OER |= PIO_PC2;
 
+    const uint8_t c = 't';
     while (1)
     {
-        /* Toggle PC2(D0) */
-        PIOC->PIO_SODR |= (1 << PIO_PC2);
-        int i = 0;
-        while (i < 10000) {i++;}
-        PIOC->PIO_CODR |= (1 << PIO_PC2);
+        // /* Toggle PC2(D0) */
+        // PIOC->PIO_SODR |= PIO_PC2;
+        // int i = 0;
+        // while (i < 10000) {i++;}
+        // PIOC->PIO_CODR |= PIO_PC2;
+        (void)uart_putchar(c);
     }
     
 }
