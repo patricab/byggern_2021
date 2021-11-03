3e0e824e5837ed53d83b19f81a4ab0b1d0b9615d
Andreas Bjørsvik <andreas.bjorsvik@gmail.com>
2021-11-03 15:12:17 +0100

    Added comments

--- node2/src/pwm.c
+++ node2/src/pwm.c
@@ -1,16 +1,16 @@
-
-
 #include <stdio.h>
 #include <stdarg.h>
 #include "pwm.h"
 #include "sam.h"
 
 
+/**
+ * @brief initialize pwm and moves the servo to the middle
+ * 
+ */
 
 void pwm_init(void){
 
-
-    
     REG_PWM_CLK = PWM_CLK_PREA(0) | PWM_CLK_DIVA(42);     // Set the PWM clock rate to 2MHz (84MHz/42) 
     PMC->PMC_PCER1 |= (1 << 4);                           // Enable PWM 
     REG_PIOC_PDR |= PIO_PDR_P19;                          // Set PWM pin to an output
@@ -19,19 +19,21 @@ void pwm_init(void){
     REG_PWM_WPCR |= PWM_WPCR_WPRG0 | PWM_WPCR_WPRG2 | PWM_WPCR_WPRG3 | PWM_WPCR_WPCMD(0); //enable write protection 
     REG_PWM_CMR5 = PWM_CMR_CALG | PWM_CMR_CPRE_CLKA;      // Enable dual slope PWM and set the clock source as CLKA
     REG_PWM_CPRD5 = 20000;                                // Set the PWM frequency 2MHz/(2 * 20000) = 50Hz 
-    REG_PWM_CDTY5 = 18500;                                 // Set the PWM duty cycle to 1500 - centre the servo
     REG_PWM_ENA = PWM_ENA_CHID5;                          // Enable the PWM channel     
-    
+    REG_PWM_CDTY5 = 18500;                                 // Set the PWM duty cycle to 1500 - centre the servo
+
     
     
     
 }
-
-//REG_PWM_WPCR = PWM_WPCR_WPKEY(5265229);  //dec values
+/**
+ * @brief Set the pwm with so the servo can move. 
+ * 
+ * @param joystic, the input must be between 0 and 255, if not the servo is set to middle value 
+ */
 
 void pwm_run(int joystic){
     
-    //REG_PWM_CDTY5 = 1500;  
     if ((joystic >= 0) && (joystic <=255)) {
         REG_PWM_CDTY5 = (20000-(900 + (joystic*4.6875)));
     }   else {
