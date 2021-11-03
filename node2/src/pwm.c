7ba70f07ca47652eaa27748d90a7832f90af7d74
Andreas Bjørsvik <andreas.bjorsvik@gmail.com>
2021-11-03 13:50:59 +0100

    Fixed PWM_init

--- node2/src/pwm.c
+++ node2/src/pwm.c
@@ -10,13 +10,13 @@
 void pwm_init(void){
 
 
+    
+    REG_PWM_CLK = PWM_CLK_PREA(0) | PWM_CLK_DIVA(42);     // Set the PWM clock rate to 2MHz (84MHz/42) 
+    PMC->PMC_PCER1 |= (1 << 4);                           // Enable PWM 
+    REG_PIOC_PDR |= PIO_PDR_P19;                          // Set PWM pin to an output
+    REG_PIOC_ABSR |= PIO_ABSR_P19;                        // Set PWM pin perhipheral type A or B, in this case B
     REG_PWM_WPCR = PWM_WPCR_WPKEY(0x50574D);              // Set protection key 
     REG_PWM_WPCR |= PWM_WPCR_WPRG0 | PWM_WPCR_WPRG2 | PWM_WPCR_WPRG3 | PWM_WPCR_WPCMD(0); //enable write protection 
-
-    PMC->PMC_PCER1 |= (1 << 4);                           // Enable PWM 
-    REG_PIOC_ABSR |= PIO_ABSR_P19;                        // Set PWM pin perhipheral type A or B, in this case B
-    REG_PIOC_PDR |= PIO_PDR_P19;                          // Set PWM pin to an output
-    REG_PWM_CLK = PWM_CLK_PREA(0) | PWM_CLK_DIVA(42);     // Set the PWM clock rate to 2MHz (84MHz/42) 
     REG_PWM_CMR0 = PWM_CMR_CALG | PWM_CMR_CPRE_CLKA;      // Enable dual slope PWM and set the clock source as CLKA
     REG_PWM_CPRD0 = 20000;                                // Set the PWM frequency 2MHz/(2 * 20000) = 50Hz 
     REG_PWM_CDTY0 = 1500;                                 // Set the PWM duty cycle to 1500 - centre the servo
