6b12913184ff2076fa5a5244a6bb17e7587b1450
eirik <eirik_skogan@hotmail.com>
2021-11-03 15:08:41 +0100

    CAN is working to node 2

--- node1/src/main.c
+++ node1/src/main.c
@@ -29,33 +29,35 @@ int main(void)
 
 	can_bus_init(); // also initialize SPI
 	can_struct can_message0;
+	can_message0.id = 13;
+	can_message0.length = 8;
+	can_message0.data[0] = 'H';
+	can_message0.data[1] = 'E';
+	can_message0.data[2] = 'L';
+	can_message0.data[3] = 'L';
+	can_message0.data[4] = 'O';
+	can_message0.data[5] = 'H';
+	can_message0.data[6] = 'E';
+	can_message0.data[7] = 'L';
+
+	uint8_t buffer_0 = 0;
 
 
     while (1) 
     {
-		can_message0.id = 1;
-		can_message0.length = 5;
-		can_message0.data[0] = 'H';
-		can_message0.data[1] = 'E';
-		can_message0.data[2] = 'L';
-		can_message0.data[3] = 'L';
-		can_message0.data[4] = 'O';
-
-		uint8_t buffer_0 = 0;
-
 		can_controller_bit_modify(MCP_RXB0CTRL, 0b00001000, 0b00000000); // reset TXB0CTRL.TXREQ
-		can_transmit(&can_message0, buffer_0);
+		//can_transmit(&can_message0, buffer_0);
 		_delay_ms(50);
 
 
-		// can_struct message0 = can_receive(0);
-		// _delay_ms(50);
-		// printf("   Data: ");
-		// for (int i = 0; i<5; i++){
-		// 	uart_tx(message0.data[i]);
-		// }
-		// printf("   ID: ");
-		// printf("%u", message0.id);
+		can_struct message0 = can_receive(0);
+		_delay_ms(50);
+		printf("   Data: ");
+		for (int i = 0; i<message0.length; i++){
+			uart_tx(message0.data[i]);
+		}
+		printf("   ID: ");
+		printf("%u", message0.id);
 
 		// MCP STATUS
 		uint8_t status = can_controller_read(MCP_READ_STATUS);
