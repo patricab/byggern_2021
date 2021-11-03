93e8377a7837fa7e3a5dedfe231d0fe5375f6dc5
patricab <patric.berthelsen@gmail.com>
2021-11-03 15:30:39 +0100

    Recieving CAN in main and sending to PWM

--- node2/src/main.c
+++ node2/src/main.c
@@ -1,15 +1,11 @@
 #include <stdio.h>
 #include <stdarg.h>
 
-// #include "sam/sam3x/include/sam3x8e.h"
-// #include "sam/sam3x/include/component/component_pio.h"
 #include "sam.h"
 #include "uart.h"
 #include <pwm.h>
 #include <printf-stdarg.h>
-
-// #include <bit.h>
-
+#include <can_controller.h>
 
 int main()
 {
@@ -17,10 +13,10 @@ int main()
 
     WDT->WDT_MR = WDT_MR_WDDIS; //Disable Watchdog Timer
 
+    /* Initialize libraries */
     configure_uart();
-    // can_test();
-    // configure_uart();
-    // pwm_init();
+    pwm_init();
+    can_init_def_tx_rx_mb();
 
     // /* Disable pull-up on bit PC2(D0) */
     // PIOA->PIO_PUDR |= PIO_PA19;
@@ -29,13 +25,9 @@ int main()
     // /* Set output enable on PC2(D0) */
     // PIOA->PIO_OER |= PIO_PA19;
     
-    //const uint8_t c = 'a';
-
-    // const uint8_t c = 's';
+    CAN_MESSAGE msg;
     while (1)
     {
-    //     //(void)uart_putchar(c);
-        
     //     /* Toggle PC2(D0) */
     //     int i = 0;
     //     while (i < 1000000) {i++;}
@@ -43,8 +35,12 @@ int main()
     //     i = 0;
     //     while (i < 1000000) {i++;}
     //     PIOA->PIO_CODR |= PIO_PA19;
-    //     // (void)uart_putchar(c);
         // printf("Test\r\n", 0);
+        
+        /* Recieve can data and send to servo */
+        can_receive(&msg, 0);
+        pwm_run((int)msg.data);
+        
     }
     
 }
