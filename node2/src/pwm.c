2724a373c47f139b273b118d490e4c199334f83a
Andreas Bjørsvik <andreas.bjorsvik@gmail.com>
2021-11-03 14:17:07 +0100

    PWM_init is working!

--- node2/src/pwm.c
+++ node2/src/pwm.c
@@ -17,9 +17,9 @@ void pwm_init(void){
     REG_PIOC_ABSR |= PIO_ABSR_P19;                        // Set PWM pin perhipheral type A or B, in this case B
     REG_PWM_WPCR = PWM_WPCR_WPKEY(0x50574D);              // Set protection key 
     REG_PWM_WPCR |= PWM_WPCR_WPRG0 | PWM_WPCR_WPRG2 | PWM_WPCR_WPRG3 | PWM_WPCR_WPCMD(0); //enable write protection 
-    REG_PWM_CMR0 = PWM_CMR_CALG | PWM_CMR_CPRE_CLKA;      // Enable dual slope PWM and set the clock source as CLKA
-    REG_PWM_CPRD0 = 20000;                                // Set the PWM frequency 2MHz/(2 * 20000) = 50Hz 
-    REG_PWM_CDTY0 = 1500;                                 // Set the PWM duty cycle to 1500 - centre the servo
+    REG_PWM_CMR5 = PWM_CMR_CALG | PWM_CMR_CPRE_CLKA;      // Enable dual slope PWM and set the clock source as CLKA
+    REG_PWM_CPRD5 = 20000;                                // Set the PWM frequency 2MHz/(2 * 20000) = 50Hz 
+    REG_PWM_CDTY5 = 1500;                                 // Set the PWM duty cycle to 1500 - centre the servo
     REG_PWM_ENA = PWM_ENA_CHID5;                          // Enable the PWM channel     
     
     
@@ -31,10 +31,8 @@ void pwm_init(void){
 
 void pwm_run(int joystic){
     
-
-
-
-    REG_PWM_CDTY0 = 1500;  
+    //REG_PWM_CDTY0 = 1500;  
+    REG_PWM_CDTY0 = (900 + (joystic*4.6875));
 
 }
 
