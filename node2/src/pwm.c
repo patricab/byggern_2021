701ba5d22a4a3c39df597ae988a29624021adba0
Andreas Bjørsvik <andreas.bjorsvik@gmail.com>
2021-11-03 13:50:30 +0100

    Fixed pwm_init

--- node2/src/pwm.c
+++ node2/src/pwm.c
@@ -9,10 +9,11 @@
 
 void pwm_init(void){
 
-    //PMC->PMC_PCER1 |= (1 << 4);
-    //REG_PMC_PCER1 |= PMC_PCER1_PID36;                     // Enable PWM 
 
-    REG_PMC_PCER1 |= (1 << 4);
+    REG_PWM_WPCR = PWM_WPCR_WPKEY(0x50574D);              // Set protection key 
+    REG_PWM_WPCR |= PWM_WPCR_WPRG0 | PWM_WPCR_WPRG2 | PWM_WPCR_WPRG3 | PWM_WPCR_WPCMD(0); //enable write protection 
+
+    PMC->PMC_PCER1 |= (1 << 4);                           // Enable PWM 
     REG_PIOC_ABSR |= PIO_ABSR_P19;                        // Set PWM pin perhipheral type A or B, in this case B
     REG_PIOC_PDR |= PIO_PDR_P19;                          // Set PWM pin to an output
     REG_PWM_CLK = PWM_CLK_PREA(0) | PWM_CLK_DIVA(42);     // Set the PWM clock rate to 2MHz (84MHz/42) 
@@ -20,12 +21,13 @@ void pwm_init(void){
     REG_PWM_CPRD0 = 20000;                                // Set the PWM frequency 2MHz/(2 * 20000) = 50Hz 
     REG_PWM_CDTY0 = 1500;                                 // Set the PWM duty cycle to 1500 - centre the servo
     REG_PWM_ENA = PWM_ENA_CHID5;                          // Enable the PWM channel     
-    //REG_PWM_WPCR = PWM_WPCR_WPKEY(5265229);  //dec values
-    REG_PWM_WPCR = PWM_WPCR_WPKEY(0x50574D);
-    REG_PWM_WPCR |= PWM_WPCR_WPRG0 | PWM_WPCR_WPRG2 | PWM_WPCR_WPRG3 | PWM_WPCR_WPCMD(0); 
+    
+    
     
     
 }
+
+//REG_PWM_WPCR = PWM_WPCR_WPKEY(5265229);  //dec values
 
 void pwm_run(int joystic){
     
