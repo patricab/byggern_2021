19b1a722f91c194e50cae41acdbbcddb2835a55a
Andreas Bjørsvik <andreas.bjorsvik@gmail.com>
2021-11-10 15:17:28 +0100

    Added solonoid

---
+++ node2/include/solonoid.h
@@ -0,0 +1,20 @@
+
+#ifndef PWM_H
+#define PWM_H
+
+/**
+ * @brief If input is high the solonoid is activated
+ * 
+ */
+void run_solonoid(bool);
+
+
+/**
+ * @brief setup the solonoid pin
+ * 
+ */
+void solonoid_setup(void);
+
+
+
+#endif 
