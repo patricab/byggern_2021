52449415eac0ac2905655f18e939a0661de61b10
patricab <patric.berthelsen@gmail.com>
2021-11-10 16:02:26 +0100

    WIP: sending button values over CAN

--- node2/src/main.c
+++ node2/src/main.c
@@ -6,11 +6,12 @@
 #include <pwm.h>
 #include <printf-stdarg.h>
 #include <can_controller.h>
+#include <test.h>
 
-void delay(int ms) {
-   int i = 0;
-   while (i < ms){i++;}
-}
+// void delay(int ms) {
+//    int i = 0;
+//    while (i < ms){i++;}
+// }
 
 int main()
 {
@@ -20,12 +21,13 @@ int main()
 
     /* Initialize libraries */
     configure_uart();
+    can_test();
     pwm_init();
 
-    uint8_t ret = can_init_def_tx_rx_mb();
-    if (ret > 0) {
-        printf("Error: could not initialize CAN", 0);
-    }
+    // uint8_t ret = can_init_def_tx_rx_mb();
+    // if (ret > 0) {
+    //     printf("Error: could not initialize CAN", 0);
+    // }
 
     // /* Disable pull-up on bit PC2(D0) */
     // PIOA->PIO_PUDR |= PIO_PA19;
@@ -34,25 +36,25 @@ int main()
     // /* Set output enable on PC2(D0) */
     // PIOA->PIO_OER |= PIO_PA19;
     
-    CAN_MESSAGE msg;
-    while (1)
-    {
-    //     /* Toggle PC2(D0) */
-    //     int i = 0;
-    //     while (i < 1000000) {i++;}
-    //     PIOA->PIO_SODR |= PIO_PA19;
-    //     i = 0;
-    //     while (i < 1000000) {i++;}
-    //     PIOA->PIO_CODR |= PIO_PA19;
-        // printf("Test\r\n", 0);
+    // CAN_MESSAGE msg;
+    // while (1)
+    // {
+    // //     /* Toggle PC2(D0) */
+    // //     int i = 0;
+    // //     while (i < 1000000) {i++;}
+    // //     PIOA->PIO_SODR |= PIO_PA19;
+    // //     i = 0;
+    // //     while (i < 1000000) {i++;}
+    // //     PIOA->PIO_CODR |= PIO_PA19;
+    //     // printf("Test\r\n", 0);
         
-        /* Recieve can data and send to servo */
-        can_receive(&msg, 0);
-        // printf("ID : %d\r\nLength: %d\r\nData: %x %x\r\n\n", msg.id, msg.data_length, msg.data[0], msg.data[1]);
-        // delay(1000000);
+    //     /* Recieve can data and send to servo */
+    //     can_receive(&msg, 0);
+    //     // printf("ID : %d\r\nLength: %d\r\nData: %x %x\r\n\n", msg.id, msg.data_length, msg.data[0], msg.data[1]);
+    //     // delay(1000000);
 
-        pwm_run((int)msg.data[1]);
+    //     pwm_run((int)msg.data[1]);
         
-    }
+    // }
     
 }
